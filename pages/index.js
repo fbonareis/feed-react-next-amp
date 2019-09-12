3816c6d1a962bd0e9cfc0f731e563d174056db43
Fernando de Bona <fbonareis@Mac-Mini-de-Fernando.local>
2019-09-11 23:41:54 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <div className="hero">
+      <h1 className="title">Sorry, we don't have news yet.</h1>
+    </div>
+  </div>
+);
+
+export default Home;
